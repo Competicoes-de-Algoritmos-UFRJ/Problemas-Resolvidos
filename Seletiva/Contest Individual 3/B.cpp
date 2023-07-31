70b37ee56b2d50d378464d095471d9390bf2ad9c
Tiago Silva <tiago.pds.silva@gmail.com>
2023-07-31 01:01:51 -0300

    Update B.cpp

--- Seletiva/Contest Individual 3/B.cpp
+++ Seletiva/Contest Individual 3/B.cpp
@@ -1,39 +1,71 @@
-#include <bits/stdc++.h>
- 
+// ⢸⣿⣿⣿⣿⠃⠄⢀⣴⡾⠃⠄⠄⠄⠄⠄⠈⠺⠟⠛⠛⠛⠛⠻⢿⣿⣿⣿⣿⣶⣤⡀⠄
+// ⢸⣿⣿⣿⡟⢀⣴⣿⡿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣸⣿⣿⣿⣿⣿⣿⣿⣷
+// ⢸⣿⣿⠟⣴⣿⡿⡟⡼⢹⣷⢲⡶⣖⣾⣶⢄⠄⠄⠄⠄⠄⢀⣼⣿⢿⣿⣿⣿⣿⣿⣿⣿
+// ⢸⣿⢫⣾⣿⡟⣾⡸⢠⡿⢳⡿⠍⣼⣿⢏⣿⣷⢄⡀⠄⢠⣾⢻⣿⣸⣿⣿⣿⣿⣿⣿⣿
+// ⡿⣡⣿⣿⡟⡼⡁⠁⣰⠂⡾⠉⢨⣿⠃⣿⡿⠍⣾⣟⢤⣿⢇⣿⢇⣿⣿⢿⣿⣿⣿⣿⣿
+// ⣱⣿⣿⡟⡐⣰⣧⡷⣿⣴⣧⣤⣼⣯⢸⡿⠁⣰⠟⢀⣼⠏⣲⠏⢸⣿⡟⣿⣿⣿⣿⣿⣿
+// ⣿⣿⡟⠁⠄⠟⣁⠄⢡⣿⣿⣿⣿⣿⣿⣦⣼⢟⢀⡼⠃⡹⠃⡀⢸⡿⢸⣿⣿⣿⣿⣿⡟
+// ⣿⣿⠃⠄⢀⣾⠋⠓⢰⣿⣿⣿⣿⣿⣿⠿⣿⣿⣾⣅⢔⣕⡇⡇⡼⢁⣿⣿⣿⣿⣿⣿⢣
+// ⣿⡟⠄⠄⣾⣇⠷⣢⣿⣿⣿⣿⣿⣿⣿⣭⣀⡈⠙⢿⣿⣿⡇⡧⢁⣾⣿⣿⣿⣿⣿⢏⣾
+// ⣿⡇⠄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢻⠇⠄⠄⢿⣿⡇⢡⣾⣿⣿⣿⣿⣿⣏⣼⣿
+// ⣿⣷⢰⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⢰⣧⣀⡄⢀⠘⡿⣰⣿⣿⣿⣿⣿⣿⠟⣼⣿⣿
+// ⢹⣿⢸⣿⣿⠟⠻⢿⣿⣿⣿⣿⣿⣿⣿⣶⣭⣉⣤⣿⢈⣼⣿⣿⣿⣿⣿⣿⠏⣾⣹⣿⣿
+// ⢸⠇⡜⣿⡟⠄⠄⠄⠈⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟⣱⣻⣿⣿⣿⣿⣿⠟⠁⢳⠃⣿⣿⣿
+// ⠄⣰⡗⠹⣿⣄⠄⠄⠄⢀⣿⣿⣿⣿⣿⣿⠟⣅⣥⣿⣿⣿⣿⠿⠋⠄⠄⣾⡌⢠⣿⡿⠃
+// ⠜⠋⢠⣷⢻⣿⣿⣶⣾⣿⣿⣿⣿⠿⣛⣥⣾⣿⠿⠟⠛⠉⠄⠄
+
+
+#include "bits/stdc++.h"
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace __gnu_pbds;
 using namespace std;
 
+#define MOD 1000000007
+#define mod(x,mvvm) (((x%mvvm)+mvvm)%mvvm)
 #define f first
 #define s second
 #define pb push_back
-#define es " "
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+#define vii vector<int>
+#define vll vector<long long>
 #define endl "\n"
+#define esp " "
 #define INF 0x3f3f3f3f
 #define INFL 0x3f3f3f3f3f3f3f3f
 #define fastio ios_base::sync_with_stdio(false), cin.tie(nullptr)
-#define sz(x) (int)x.size()
-#define all(x) x.begin(), x.end()
 typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<int, pii> pip;
 typedef unsigned long long ull;
-const int mxn = 1e5+5;
-const int mxm = 1e5+5;
-const int MOD = 1e9+7;
+typedef long double ld;
+typedef tree<int,null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef tree<int,null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> multiordered_set;  //--set.lower_bound(value) ao inves de find
 
-int t, a, b, c;
+int solve(){
+
+    int n; cin >> n;
+    vector<int> arr(26,0);
+    int ans = 0;
+
+    string str; cin >> str;
+    for(char c: str){
+        ans = max(c-'a'+1, ans);
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
 
 int main(){
     fastio;
+
+    int t = 1;
     cin >> t;
     while(t--){
-        int n;
-        string a;
-        cin >> n >> a;
-        int maior=0;
-        for(char i:a){
-            maior=max(maior, i-'a'+1);
-        }
-        cout << maior << endl;
-    }
+        solve();
+    }    
+
     return 0;
 }
