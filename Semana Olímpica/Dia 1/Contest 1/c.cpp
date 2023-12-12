0c5337747a04d6e85235581b18378d564c361e01
Tiago Silva <tiago.pds.silva@gmail.com>
2023-12-12 13:14:32 -0300

    Create c.cpp

---
+++ Semana Olímpica/Dia 1/Contest 1/c.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    for(int i=0; i<5; i++){
+        int x;
+        cin >> x;
+
+        if(x==0){
+            cout << i+1 << endl;
+        }
+    }
+
+    return 0;
+}
