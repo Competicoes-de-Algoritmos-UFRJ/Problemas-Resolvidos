3946e1cfc5102e1098ad42a7a0853c077301b65e
Tiago Silva <tiago.pds.silva@gmail.com>
2023-12-12 19:40:03 -0300

    Update n.cpp

--- Semana Olímpica/Dia 1/Contest 1/n.cpp
+++ Semana Olímpica/Dia 1/Contest 1/n.cpp
@@ -11,21 +11,23 @@ int main(){
 
         cin >> a >> b >> c >> x >> y;
 
-        if(x>=a){
+        if(x>=a){ //se o número de cachorros, for maior que o número de rações para cachorros, então diminuo o número de cachorros pelo número de rações
             x-=a;
-            a=0;
-        }else{
+        }else{ //caso contrário, consigo alimentar todos os cachorros com as rações específicas
             x=0;
         }
 
-        if(y>=b){
+        // o if de cima, poderia ser substituído por x = max(0, x - a);
+
+        if(y>=b){ //se o número de gatos, for maior que o número de rações para gatos, então diminuo o número de gatos pelo número de rações
             y-=b;
-            b=0;
-        }else{
+        }else //caso contrário, consigo alimentar todos os gatos com as rações específicas
             y=0;
         }
 
-        if(x+y<=c){
+        // o if de cima, poderia ser substituído por y = max(0, y - b);
+
+        if(x+y<=c){ //se o número de cachorros mais o número de gatos que sobrar for menor ou igual ao número de rações gerais, então é possível
             cout << "YES" << endl;
         }else{
             cout << "NO" << endl;
