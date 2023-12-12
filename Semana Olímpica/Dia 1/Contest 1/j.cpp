1a261faf58145892be25b67450c2ec7cad299ec0
Tiago Silva <tiago.pds.silva@gmail.com>
2023-12-12 19:46:01 -0300

    Update j.cpp

--- Semana Olímpica/Dia 1/Contest 1/j.cpp
+++ Semana Olímpica/Dia 1/Contest 1/j.cpp
@@ -4,17 +4,20 @@ using namespace std;
 
 int n, c[200005], r[200005];
 
+//a função acha_maior é uma função que dada uma cor de parâmetro, ela acha a posição da pessoa que possui essa cor, e tem o maior rank
+//se não houver pessoas com a cor a função retorna -1
 int acha_maior(int cor){
     int maior = 0, pos = -1;
 
     for(int i=0; i<n; i++){
-        if(c[i]==cor){
-            if(r[i]>maior){
+        if(c[i]==cor){ //se a pessoa tiver a cor que eu quero
+            if(r[i]>maior){ //e se o rank dela for maior que o rank atual, então faço a atualização do maior, e da posição, onde está o maior
                 maior = r[i];
                 pos = i;
             }
         }
     }
+    
     return pos;
 }
 
@@ -35,7 +38,7 @@ int main(){
 
     if(resp!=-1){
         cout << resp+1 << endl;
-    }else{
+    }else{ // se não houver pessoas com a cor t, então devemos procurar pela cor da primeira pessoa
         cout << acha_maior(c[0])+1 << endl;
     }
 
