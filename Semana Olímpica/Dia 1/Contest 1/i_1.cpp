5b63f4758485160506615ac2b73dfea8efbb3030
Tiago Silva <tiago.pds.silva@gmail.com>
2023-12-12 19:48:47 -0300

    Update i_1.cpp

--- Semana Olímpica/Dia 1/Contest 1/i_1.cpp
+++ Semana Olímpica/Dia 1/Contest 1/i_1.cpp
@@ -13,6 +13,7 @@ int main(){
 
         vector<int> v;
 
+        //para esse problema precisamos saber quem é o maior valor e o segundo maior valor
         int maior = 0, seg_maior = 0;
 
         for(int j=0; j<n; j++){
@@ -20,16 +21,16 @@ int main(){
             cin >> x;
             v.push_back(x);
 
-            if(x>maior){
+            if(x>maior){ // se x é maior que o maior valor até o momento, então o maior se torna o segundo maior, e x se torna o maior
                 seg_maior = maior;
                 maior = x;
-            }else if(x>seg_maior){
+            }else if(x>seg_maior){ //se x não for maior que o maior valor até o momento, mas foi maior que o segundo maior, então ele se torna o segundo maior
                 seg_maior = x;
             }
         }
 
         for(int i:v){
-            if(i==maior){
+            if(i==maior){ //se o número atual for igual ao maior valor, então devo fazer a diferença com o segundo maior valor
                 cout << i - seg_maior << ' ';
             }else{
                 cout << i - maior << ' ';
