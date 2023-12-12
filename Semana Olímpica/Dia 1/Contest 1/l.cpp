48ce4e64729d3d23f047891fe202f04c68388d79
Tiago Silva <tiago.pds.silva@gmail.com>
2023-12-12 19:42:28 -0300

    Update l.cpp

--- Semana Olímpica/Dia 1/Contest 1/l.cpp
+++ Semana Olímpica/Dia 1/Contest 1/l.cpp
@@ -6,16 +6,17 @@ int main(){
     int n;
     cin >> n;
 
+    //o vetor called irá guardar a informação se a pessoa i foi chamada ou não, se called[i] for igual a 1, isso quer dizer que a pessoa i foi chamada, caso contrário não
     int v[n], called[n];
 
     for(int i=0; i<n; i++){
         cin >> v[i];
 
-        called[i] = 0;
+        called[i] = 0; //inicializo todas as posições do called como 0, pois nenhuma pessoa foi chamada ainda
     }
 
     for(int i=0; i<n; i++){
-        if(!called[i]){
+        if(!called[i]){ //se a pessoa atual não foi chamada, então ela precisa chamar outra pessoa
             called[v[i]-1] = 1;
         }
     }
