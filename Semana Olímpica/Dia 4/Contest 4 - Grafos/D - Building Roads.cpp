318b0edf6dbed8918de1834b22ebd0130fad130b
MHJMBS <mhjmbs@hotmail.com>
2023-12-15 00:26:07 -0300

    Add some solutions to Semana Olímpica 2023's fourth day

---
+++ Semana Olímpica/Dia 4/Contest 4 - Grafos/D - Building Roads.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+vector<vector<int>> adjacencies;
+vector<bool> vis;
+
+void dfs(int node) {
+    vis[node] = true;
+
+    for(int adj : adjacencies[node]) {
+        if(!vis[adj]) {
+            dfs(adj);
+        }
+    }
+}
+
+int main() {
+    fastio;
+
+    int n, m;
+    cin >> n >> m;
+
+    adjacencies.resize(n);
+    vis.resize(n, false);
+
+    for(int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        u--; v--;
+        adjacencies[u].push_back(v);
+        adjacencies[v].push_back(u);
+    }
+
+    int cc = 0; // number of connected components
+    vector<int> ans; // ans[i] := first node we visit from the i-th connected component
+
+    for(int node = 0; node < n; node++) {
+        if(!vis[node]) {
+            ans.push_back(node);
+            cc++;
+            dfs(node);
+        }
+    }
+
+    cout << cc-1 << '\n';
+    for(int i = 1; i < ans.size(); i++) {
+        cout << ans[i-1]+1 << ' ' << ans[i]+1 << '\n';
+    }
+}
---
+++ Semana Olímpica/Dia 4/Exercícios Aula 4/B - Labyrinth.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fastio ios::sync_with_stdio(0), cin.tie(nullptr)
+
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using tiii = tuple<int,int,int>;
+using tlll = tuple<ll,ll,ll>;
+
+using ordered_set = tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+using ordered_multiset = tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int n, m;
+vector<string> grid;
+
+queue<pair<pii,int>> q;
+vector<vector<bool>> enqueued;
+vector<vector<int>> dist; //dist[i][j] := distance traversed from A to position (i,j)
+vector<vector<char>> lastMove; //lastMove[i][j] := move used to discover position (i,j) in BFS, either R, D, L, U
+vector<int> di = {0, 1, 0, -1}, dj = {1, 0, -1, 0}; //variations of coordinates in moes R, D, L, U. (i+di[0], j+dj[0]) = (i, j+1) => move to the right
+
+pii getCoord(char c) {
+    for(int i = 0; i < grid.size(); i++) {
+        for(int j = 0; j < grid[i].size(); j++) {
+            if(grid[i][j] == c) return {i,j};
+        }
+    }
+}
+
+bool inbounds(int i, int j) {
+    return (0 <= i && i < n) && (0 <= j && j < m);
+}
+
+int main() {
+    fastio;
+
+    cin >> n >> m;
+
+    grid.resize(n);
+    for(string& line : grid) cin >> line;
+
+    pii A = getCoord('A');
+    pii B = getCoord('B');
+
+    enqueued.resize(n, vector<bool>(m, false));
+    dist.resize(n, vector<int>(m, -1));
+    lastMove.resize(n, vector<char>(m));
+
+    dist[A.first][A.second] = 0;
+    q.emplace(A, 0);
+    enqueued[A.first][A.second] = true;
+
+    while(!q.empty()) {
+        auto [i,j] = q.front().first;
+        int currDist = q.front().second;
+        q.pop();
+        
+        dist[i][j] = currDist;
+
+        for(int k = 0; k < 4; k++) {
+            int ni = i + di[k];
+            int nj = j + dj[k];
+
+            if(inbounds(ni, nj) && grid[i][j] != '#' && !enqueued[ni][nj]) {
+                q.emplace(make_pair(ni,nj), currDist+1);
+                enqueued[ni][nj] = true;
+
+                if(k == 0) lastMove[ni][nj] = 'R';
+                else if(k == 1) lastMove[ni][nj] = 'D';
+                else if(k == 2) lastMove[ni][nj] = 'L';
+                else  lastMove[ni][nj] = 'U';
+            }
+        }
+    }
+
+    if(dist[B.first][B.second] == -1) {
+        cout << "NO\n";
+        return 0;
+    }
+
+    cout << "YES\n";
+    cout << dist[B.first][B.second] << '\n';
+
+    string path;
+    pii curr = B;
+
+    while(curr != A) {
+        char move = lastMove[curr.first][curr.second];
+        path.push_back(move);
+
+        if(move == 'R') curr.second--;
+        else if(move == 'D') curr.first--;
+        else if(move == 'L') curr.second++;
+        else if(move == 'U') curr.first++;
+    }
+
+    reverse(path.begin(), path.end());
+    
+    cout << path << '\n';
+}
