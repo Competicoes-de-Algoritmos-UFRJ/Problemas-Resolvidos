789cbfda2005af913c4507bae6933e44616c3d5e
Junjeeeee <94764591+Junjeeeee@users.noreply.github.com>
2024-03-11 21:41:53 -0300

    Create a.cpp

---
+++ Treinamento de Férias/Dia 1 (Básico c++)/a.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    int a,b,c; cin >> a >> b >> c; //declaro e recebo os três números
+    cout << a+b+c << '\n'; //faço output da soma deles
+    return 0;
+}
